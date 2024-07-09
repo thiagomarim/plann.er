b1e9f8c030abc93101fca0e702f803d51f409187
Thiago Marim <thiago.marim2005@gmail.com>
2024-07-09 20:02:07 -0300

    refactor: destructure destination and date header

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,9 +1,10 @@
-import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
+import { Plus } from "lucide-react";
 import { useState } from "react";
 import { CreateActivityModal } from "./create-activity-modal";
 import { ImportantLinks } from "./important-links";
 import { Guests } from "./guests";
 import { Activities } from "./activities";
+import { DestinationAndDateHeader } from "./destination-and-date-header";
 
 export function TripDetailsPage() {
   const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
@@ -19,26 +20,7 @@ export function TripDetailsPage() {
 
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-      <div className="flex items-center justify-between h-16 px-4 rounded-xl bg-zinc-900 shadow-shape">
-        <div className="flex items-center gap-2">
-          <MapPin className="size-5 text-zinc-400" />
-          <span className=" text-zinc-100">Florianópolis, Brasil</span>
-        </div>
-
-        <div className="flex items-center gap-5">
-          <div className="flex items-center gap-2">
-            <Calendar className="size-5 text-zinc-400" />
-            <span className=" text-zinc-100">17 a 23 de Agosto</span>
-          </div>
-
-          <div className="w-px h-6 bg-zinc-800" />
-
-          <button className="flex items-center gap-2 px-5 py-2 font-medium rounded-lg bg-zinc-800 text-zinc-200 hover:bg-zinc-700">
-            Alterar local/data
-            <Settings2 className="size-5" />
-          </button>
-        </div>
-      </div>
+      <DestinationAndDateHeader />
 
       <main className="flex gap-16 px-4">
         <div className="flex-1 space-y-6">
